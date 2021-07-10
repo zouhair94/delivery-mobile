a4b77e997850763bd3b3bc99a546be97776cfe4c
zouhair94 <z.ridaoui94@outlook.com>
2021-07-10 14:54:55 +0300

    finished order page

--- src/app/order/main/main.component.ts
+++ src/app/order/main/main.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Page } from '@nativescript/core';
-
-
+import { registerElement } from "@nativescript/angular";
+import { Mapbox, MapboxViewApi, MapboxMarker } from '@nativescript-community/ui-mapbox';
+registerElement("Mapbox", () => require("@nativescript-community/ui-mapbox").MapboxView);
+import { CardView } from '@nstudio/nativescript-cardview';
+registerElement('CardView', () => CardView);
 @Component({
 	moduleId: module.id,
 	selector: 'main',
@@ -11,9 +14,26 @@ import { Page } from '@nativescript/core';
 
 export class MainComponent implements OnInit {
 
+	//map variables
+    Map: MapboxViewApi;
+    Marker: MapboxMarker;
+
 	constructor(private page: Page) { }
 
 	ngOnInit() {
 		this.page.actionBarHidden = true
+
 	 }
+
+	 onMapReady(args: any) {
+        this.Map = args.map;
+  
+        args.map.setCenter(
+            {
+                animated: true, // default true
+                zoomLevel: 14
+            }
+        );
+        
+    }
 }
